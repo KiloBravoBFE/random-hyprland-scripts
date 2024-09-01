ed64cfecb502cee33e95bf630673c033f71da205
Kristian Banze <kristian.banze@gmail.com>
2024-09-02 00:12:42 +0200

    Add custom VPN
    One can now connect to any VPN added to the system.
    
    (As long as it is possible to do so with NM...)

--- vpn.sh
+++ vpn.sh
@@ -22,13 +22,39 @@ elif [[ $1 = "-defdn" ]]; then
        nmcli connection up "$currSSID"
        echo "++ You should now be disconnected and IPv6 should be active again."
 
+elif [[ $1 = "-up" ]] && [[ $2 != "" ]]; then
+       currCon="$(nmcli -t con | head -n 1)"
+       IFS=: read -r currSSID param <<< $currCon
+       if [[ "$currSSID" == "$2" ]]; then
+              nmcli con down id "$2"
+              currCon="$(nmcli -t con | head -n 1)"
+              IFS=: read -r currSSID param <<< $currCon
+              echo "++ Already connected to VPN, disconnecting."
+       fi
+       sudo nmcli con modify "$currSSID" ipv6.method 'disabled'
+       nmcli connection up "$currSSID"
+       nmcli con up id Skara
+       echo "++ You should now be connected and IPv6 should be disabled."
+
+elif [[ $1 = "-down" ]] && [[ $2 != "" ]]; then
+       nmcli con down id "$2"
+       currCon="$(nmcli -t con | head -n 1)"
+       IFS=: read -r currSSID param <<< $currCon
+       sudo nmcli con modify "$currSSID" ipv6.method 'auto'
+       nmcli connection up "$currSSID"
+       echo "++ You should now be disconnected and IPv6 should be active again."
+
 elif [[ $1 = "-h" ]]; then
-       echo "
+       echo '
               You may use the following params:
               
-              -def To connect to default VPN (id:Skara)
-              -defdn To disconnect from default VPN (id:Skara)
-       "
+                -up   $your-vpn-id          To connect to custom VPN
+                -down $your-vpn-id          To disconnect from custom VPN
+
+                -def                        To connect to default VPN (id:Skara)
+                -defdn                      To disconnect from default VPN (id:Skara)
+
+       '
 
 
 else
