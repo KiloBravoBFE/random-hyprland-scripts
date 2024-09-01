47fb0e1e710b9a4b469c5f90e5720adf80d33fd8
Kristian Banze <kristian.banze@gmail.com>
2024-09-02 00:01:55 +0200

    Fix issue with VPN not being active due to IPv6
    In order to activate the change regarding the usage of IPv6, the module has to be rebooted.
    
    However, VPNs disconnect when the connections stops. Thus, the order has been changed.
    
    Also there is no useless attempt to turn off the VPN when it is not even on.
    
    Also there are now somewhat helpful output messages.

--- vpn.sh
+++ vpn.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 if [[ $1 = "-def" ]]; then
-       nmcli con down id Skara
        currCon="$(nmcli -t con | head -n 1)"
        IFS=: read -r currSSID param <<< $currCon
-       #literalSSID="'"$currSSID"'"'
-       #echo $currSSID
-       #echo $literalSSID
-       nmcli con up id Skara
+       if [[ "$currSSID" == "Skara" ]]; then
+              nmcli con down id Skara
+              currCon="$(nmcli -t con | head -n 1)"
+              IFS=: read -r currSSID param <<< $currCon
+              echo "++ Already connected to VPN, disconnecting."
+       fi
        sudo nmcli con modify "$currSSID" ipv6.method 'disabled'
-       #nmcli con modify 'FRITZ!Box 7490' ipv6.method 'disabled'
+       nmcli connection up "$currSSID"
+       nmcli con up id Skara
+       echo "++ You should now be connected and IPv6 should be disabled."
 
 elif [[ $1 = "-defdn" ]]; then
        nmcli con down id Skara
        currCon="$(nmcli -t con | head -n 1)"
        IFS=: read -r currSSID param <<< $currCon
        sudo nmcli con modify "$currSSID" ipv6.method 'auto'
+       nmcli connection up "$currSSID"
+       echo "++ You should now be disconnected and IPv6 should be active again."
 
 elif [[ $1 = "-h" ]]; then
        echo "
